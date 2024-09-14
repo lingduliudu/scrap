7dea7aa57d40dd97994b44aa062f54c622b53be4
lingduliudu <lingduliudu@163.com>
2024-09-14 13:53:46 +0800

    12

--- websocket/OkHttpWebSocketListener.java
+++ websocket/OkHttpWebSocketListener.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+import java.io.IOException;
 
 @Slf4j
 public class OkHttpWebSocketListener extends WebSocketListener {
@@ -12,9 +15,18 @@ public class OkHttpWebSocketListener extends WebSocketListener {
         log.info("接收到消息:"+message);
     }
     @Override
+    public void onMessage(WebSocket webSocket, ByteString byteString){
+        try {
+            String message = new String(InternalUtils.decode(byteString.toByteArray()));
+            log.info("接收到消息:"+ message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
     public void onOpen(WebSocket webSocket, Response response){
         log.info("成功");
-        webSocket.send("{\"method\":\"RSUBSCRIPTION\",\"params\":[\"main-site@crypto_price_5s@{}@normal\",\"1\"]}");
+        //webSocket.send("{\"method\":\"RSUBSCRIPTION\",\"params\":[\"main-site@crypto_price_5s@{}@normal\",\"1\"]}");
     }
     @Override
     public void onClosed(WebSocket webSocket,int code,String reason){
